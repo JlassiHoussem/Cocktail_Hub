550b19d8f7c07553ecccf78c5795d95859a53685
JlassiHoussem <114109501+JlassiHoussem@users.noreply.github.com>
2022-11-20 21:02:19 +0100

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+    return (
+      <section className="section about-section">
+          <h1 className="section-title" >About Us</h1>
+          <p>
+              {" "}
+            Nulla et sit adipisicing pariatur sunt et commodo. Eiusmod aliquip nulla commodo mollit pariatur do amet cillum nulla culpa qui cillum. Commodo occaecat esse magna proident officia culpa amet amet ea. Sint laboris duis et in esse id enim incididunt officia qui exercitation aliqua culpa. Exercitation proident nisi cillum ipsum aute in in ea minim cupidatat nulla officia pariatur.
+            Laboris consequat consequat magna elit dolore esse qui Lorem proident. Veniam labore nostrud proident culpa officia laboris minim duis quis pariatur adipisicing ut duis elit. Ex velit exercitation Lorem occaecat. In enim nisi aliquip velit do et adipisicing adipisicing velit.Consectetur culpa nulla ut veniam. Nisi sit minim commodo culpa anim adipisicing qui in officia. Cupidatat est eu voluptate ullamco nulla voluptate consequat.
+
+           </p>
+      </section>
+    );
+}
+
+export default About
